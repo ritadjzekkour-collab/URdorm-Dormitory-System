83199f48512850598cbf9087581aaddd2f4c7de5
ritadjzekkour-collab <ritadj.zekkour@ensia.edu.dz>
2026-06-20 11:29:40 -0700

    Add files via upload

---
+++ LoginDialog.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QMovie>
+#include <QGraphicsDropShadowEffect>
+
+class LoginDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit LoginDialog(QWidget* parent = nullptr);
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
+private slots:
+    void tryLogin();
+    void onLoginClicked();
+
+private:
+    QLabel*     bgLabel;
+    QLabel*     logoLabel;
+    QLabel*     titleLabel;
+    QLineEdit*  idField;
+    QLineEdit*  passField;
+    QComboBox*  roleBox;
+    QPushButton* loginBtn;
+    QLabel*     errorLabel;
+    QMovie*     bgMovie;
+
+    void setupUi();
+    void applyTheme();
+    void animateBounce();
+    void setupButtonHoverEffects();
+};
